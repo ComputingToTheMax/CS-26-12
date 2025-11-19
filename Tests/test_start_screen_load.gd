09991d32b445a28c24d8d247b96288185e932700
ChristianBuenafe <145249520+ChristianBuenafe@users.noreply.github.com>
2025-11-18 19:36:45 -0800

    Fix failing unit tests test_start_screen_load.gd

--- Tests/test_start_screen_load.gd
+++ Tests/test_start_screen_load.gd
@@ -1,26 +1,29 @@
 extends GutTest
 
-var scene_path := "res://StartScreen.tscn"
+var scenePath: String = "res://StartScreen.tscn"
+var sceneInstance: Node = null
 
-func before_each():
-    var scene := load(scene_path)
-    assert_not_null(scene, "Scene failed to load")
-    self.scene_instance = scene.instantiate()
+func before_each() -> void:
+	var scene: PackedScene = load(scenePath)
+	assert_not_null(scene, "Scene failed to load")
+	sceneInstance = scene.instantiate()
+	assert_not_null(sceneInstance, "Scene failed to instantiate")
 
-func after_each():
-    self.scene_instance.queue_free()
+func after_each() -> void:
+	if sceneInstance:
+		sceneInstance.queue_free()
 
-func test_scene_has_background():
-    var node := scene_instance.get_node("Background")
-    assert_not_null(node, "Background node missing")
-    assert_true(node is Sprite2D, "Background is not Sprite2D")
+func test_scene_has_background() -> void:
+	var node: Node = sceneInstance.get_node("Background")
+	assert_not_null(node, "Background node missing")
+	assert_true(node is Sprite2D, "Background is not Sprite2D")
 
-func test_scene_has_character():
-    var node := scene_instance.get_node("Character")
-    assert_not_null(node, "Character node missing")
-    assert_true(node is Sprite2D, "Character is not Sprite2D")
+func test_scene_has_character() -> void:
+	var node: Node = sceneInstance.get_node("Character")
+	assert_not_null(node, "Character node missing")
+	assert_true(node is Sprite2D, "Character is not Sprite2D")
 
-func test_scene_has_play_button():
-    var node := scene_instance.get_node("PlayButton")
-    assert_not_null(node, "PlayButton node missing")
-    assert_true(node is Button, "PlayButton is not a Button")
+func test_scene_has_play_button() -> void:
+	var node: Node = sceneInstance.get_node("PlayButton")
+	assert_not_null(node, "PlayButton node missing")
+	assert_true(node is Button, "PlayButton is not a Button")
