34ef540fd4518f64ae2db184807aad96e999be3d
ChristianBuenafe <145249520+ChristianBuenafe@users.noreply.github.com>
2025-11-18 19:37:00 -0800

    Fix failing unit tests test_start_screen_properties.gd

--- Tests/test_start_screen_properties.gd
+++ Tests/test_start_screen_properties.gd
@@ -1,26 +1,29 @@
 extends GutTest
 
-var scene_path := "res://StartScreen.tscn"
+var scenePath: String = "res://StartScreen.tscn"
+var sceneInstance: Node = null
 
-func before_each():
-    var scene := load(scene_path)
-    self.scene_instance = scene.instantiate()
+func before_each() -> void:
+    var scene: PackedScene = load(scenePath)
+    assert_not_null(scene, "Scene failed to load")
+    sceneInstance = scene.instantiate()
+    assert_not_null(sceneInstance, "Scene failed to instantiate")
 
-func after_each():
-    self.scene_instance.queue_free()
+func after_each() -> void:
+    if sceneInstance:
+        sceneInstance.queue_free()
 
-func test_background_texture_loaded():
-    var bg := scene_instance.get_node("Background")
+func test_background_texture_loaded() -> void:
+    var bg: Sprite2D = sceneInstance.get_node("Background")
+    assert_not_null(bg, "Background node missing")
     assert_not_null(bg.texture, "Background texture not assigned")
 
-func test_character_texture_loaded():
-    var char := scene_instance.get_node("Character")
-    assert_not_null(char.texture, "Character texture not assigned")
-
-func test_background_position():
-    var bg := scene_instance.get_node("Background")
+func test_background_position() -> void:
+    var bg: Sprite2D = sceneInstance.get_node("Background")
+    assert_not_null(bg, "Background node missing")
     assert_eq(bg.position, Vector2(640, 360), "Background position incorrect")
 
-func test_character_position():
-    var char := scene_instance.get_node("Character")
+func test_character_position() -> void:
+    var char: Sprite2D = sceneInstance.get_node("Character")
+    assert_not_null(char, "Character node missing")
     assert_eq(char.position, Vector2(640, 400), "Character position incorrect")
