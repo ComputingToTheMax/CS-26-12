081c85f73d452311bea259421f94764c9ea12907
Avery Feingold <55460590+AveryFeingold@users.noreply.github.com>
2025-12-01 22:48:40 -0800

    Implement click sound for button interactions
    Added audio feedback for button presses in the Start Screen.

--- Scripts/StartScreen.gd
+++ Scripts/StartScreen.gd
@@ -1,6 +1,18 @@
 extends Node2D
 
+var click_player: AudioStreamPlayer
+
 func _ready():
+	# Create the audio player
+	click_player = AudioStreamPlayer.new()
+	add_child(click_player)
+
+	# Load the click sound file
+	click_player.stream = load("res://Sources/Sounds/click.wav")
+
+	# Return to Main Menu button
+	$Background/ReturnButton.pressed.connect(_on_return_pressed)
+
 	# Play button
 	$PlayButton.pressed.connect(_on_play_pressed)
 
@@ -16,16 +28,22 @@ func _ready():
 
 
 func _on_play_pressed():
+	click_player.play()
+	await get_tree().create_timer(click_player.stream.get_length()).timeout
 	var next_scene = load("res://Tutorial_Screen.tscn")
 	get_tree().change_scene_to_packed(next_scene)
 
 
 func _on_settings_pressed():
+	click_player.play()
+	await get_tree().create_timer(click_player.stream.get_length()).timeout
 	var next_scene = load("res://Settings_Screen.tscn")
 	get_tree().change_scene_to_packed(next_scene)
 
 
 func _on_external_link_pressed():
+	click_player.play()
+	await get_tree().create_timer(click_player.stream.get_length()).timeout
 	OS.shell_open("https://psyche.ssl.berkeley.edu/mission/faq/")
 
 
