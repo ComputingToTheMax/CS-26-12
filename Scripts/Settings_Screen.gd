8681c1a38810984210da87e3ccf97e86d6924174
Avery Feingold <55460590+AveryFeingold@users.noreply.github.com>
2025-12-01 22:47:31 -0800

    Implement click sound for button presses
    Added audio feedback for button interactions.

--- Scripts/Settings_Screen.gd
+++ Scripts/Settings_Screen.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
+var click_player: AudioStreamPlayer
+
 func _ready():
+	# Create the audio player
+	click_player = AudioStreamPlayer.new()
+	add_child(click_player)
+
+	# Load the click sound file
+	click_player.stream = load("res://Sources/Sounds/click.wav")
+
 	# Return to Main Menu button
 	$Background/ReturnButton.pressed.connect(_on_return_pressed)
 
@@ -12,8 +21,12 @@ func _ready():
 
 func _on_return_pressed():
 	# Use correct scene-changing method
+	click_player.play()
+	await get_tree().create_timer(click_player.stream.get_length()).timeout
 	get_tree().change_scene_to_file("res://StartScreen.tscn")
 
 func _change_resolution(height: int, width: int) -> void:
+	click_player.play()
+	await get_tree().create_timer(click_player.stream.get_length()).timeout
 	DisplayServer.window_set_size(Vector2(width, height))
 	print("Resolution changed to: %dx%d" % [width, height])
