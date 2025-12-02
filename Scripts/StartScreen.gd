b29f10959f8068bbb09042e9d024593610d491c3
Aiden Bak <aidenbak@hotmail.com>
2025-12-02 15:56:57 -0800

    Added transition between the start screen and the tutorial screen. Changed StartScreen.tscn

--- Scripts/StartScreen.gd
+++ Scripts/StartScreen.gd
@@ -1,7 +1,10 @@
 extends Node2D
-
+@onready var fade_rect: ColorRect =$FadeLayer/FadeRect
 func _ready():
 	# Play button
+	fade_rect.color.a=1.0
+	var tween := create_tween()
+	tween.tween_property(fade_rect,"color:a",0.0,0.4)
 	$PlayButton.pressed.connect(_on_play_pressed)
 
 	# Settings button
@@ -13,16 +16,21 @@ func _ready():
 
 	# Credits button
 	$Button.pressed.connect(_on_credits_pressed)
-
-
-func _on_play_pressed():
-	var next_scene = load("res://Tutorial_Screen.tscn")
+func _transition_to_scene(path: String) ->void:
+	var tween := create_tween()
+	tween.tween_property(fade_rect,"color:a",1.0,0.4)
+	await tween.finished
+	
+	var next_scene: PackedScene=load(path)
 	get_tree().change_scene_to_packed(next_scene)
+
+func _on_play_pressed() -> void:
+	_transition_to_scene("res://Tutorial_Screen.tscn")
 
 
 func _on_settings_pressed():
-	var next_scene = load("res://Settings_Screen.tscn")
-	get_tree().change_scene_to_packed(next_scene)
+	_transition_to_scene("res://Settings_Screen.tscn")
+	
 
 
 func _on_external_link_pressed():
@@ -30,5 +38,5 @@ func _on_external_link_pressed():
 
 
 func _on_credits_pressed():
-	var credits_scene = load("res://Credits.tscn")
-	get_tree().change_scene_to_packed(credits_scene)
+	_transition_to_scene("res://Credits.tscn")
+	
