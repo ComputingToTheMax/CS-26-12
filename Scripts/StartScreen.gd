7401d95379dba5b39fc7dd09ce30100f90e1ed44
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2025-12-04 19:29:07 -0800

    Update menu scene script to respond to signaled buttons in "StartScene.gd".

--- Scripts/StartScreen.gd
+++ Scripts/StartScreen.gd
@@ -1,7 +1,9 @@
-extends Node2D
+extends Control
 @onready var fade_rect: ColorRect =$FadeLayer/FadeRect
 
 var click_player: AudioStreamPlayer
+
+signal request_transition
 
 func _ready():
 	# Create the audio player
@@ -11,52 +13,31 @@ func _ready():
 	# Load the click sound file
 	click_player.stream = load("res://Sources/Sounds/click.wav")
 
-	# Return to Main Menu button
-
-	# Play button
 	fade_rect.color.a=1.0
 	var tween := create_tween()
 	tween.tween_property(fade_rect,"color:a",0.0,0.4)
-	$PlayButton.pressed.connect(_on_play_pressed)
-
-	# Settings button
-	$Background/SettingsButton.pressed.connect(_on_settings_pressed)
 
 	# Link to external Psyche Resources
-	if $Background.has_node("PsycheButton"):
-		$Background/PsycheButton.pressed.connect(_on_external_link_pressed)
+	$PsycheMissionLink.pressed.connect(_on_external_link_pressed)
 
-	# Credits button
-	$Button.pressed.connect(_on_credits_pressed)
-func _transition_to_scene(path: String) ->void:
-	var tween := create_tween()
-	tween.tween_property(fade_rect,"color:a",1.0,0.4)
-	await tween.finished
 	
-	var next_scene: PackedScene=load(path)
-	get_tree().change_scene_to_packed(next_scene)
-func _click_then_transition(path: String):
-	if click_player and click_player.stream:
-		click_player.play()
-		await get_tree().create_timer(click_player.stream.get_length()).timeout
-	await _transition_to_scene(path)
-	
-
-func _on_play_pressed() -> void:
-	await _click_then_transition("res://Tutorial_Screen.tscn")
-
-
-func _on_settings_pressed():
-	await _click_then_transition("res://Settings_Screen.tscn")
-
-
-
 func _on_external_link_pressed():
 	click_player.play()
 	await get_tree().create_timer(click_player.stream.get_length()).timeout
 	OS.shell_open("https://psyche.ssl.berkeley.edu/mission/faq/")
-
-
-func _on_credits_pressed():
-	await _click_then_transition("res://Credits.tscn")
 	
+# Handle General Button Transitions to New Scenes
+func _transition_to_scene(target_scene: PackedScene) ->void:
+	var tween := create_tween()
+	tween.tween_property(fade_rect,"color:a",1.0,0.4)
+	await tween.finished
+	get_tree().change_scene_to_packed(target_scene)
+	
+func _click_then_transition(target_scene: PackedScene):
+	if click_player and click_player.stream:
+		click_player.play()
+		await get_tree().create_timer(click_player.stream.get_length()).timeout
+	await _transition_to_scene(target_scene)
+
+func _on_request_transition(target_scene: PackedScene) -> void:
+	await _click_then_transition(target_scene)
