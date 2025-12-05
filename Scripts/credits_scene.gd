0b15f0f30c9f0515ba61ed4c25ba2c736acebffe
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2025-12-04 19:50:52 -0800

    Rename the "Credits.tscn" scene to "credits_scene.gd" in accordance with the recommended Godot naming conventions.

--- Scripts/credits_scene.gd
+++ Scripts/credits_scene.gd
@@ -4,5 +4,5 @@ func _ready():
 	$Background/ReturnButton.pressed.connect(_on_return_pressed)
 
 func _on_return_pressed():
-	var start_scene = load("res://StartScreen.tscn")
+	var start_scene = load("res://Scenes/main_menu.tscn")
 	get_tree().change_scene_to_packed(start_scene)
