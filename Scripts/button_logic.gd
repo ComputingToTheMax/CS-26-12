b6cfe9b22e2b8af1ff6b1b5d5ac1b634c8674fd7
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2025-12-05 22:20:04 -0800

    Update the button logic script to support direct scene changes without calling a third party in "button_logic.gd".

--- Scripts/button_logic.gd
+++ Scripts/button_logic.gd
@@ -12,7 +12,10 @@ func _ready() -> void:
 	
 	# Automatically connect any buttons this script is applied to to the _on_pressed method
 	# to handle button presses.
-	self.pressed.connect(_on_pressed);
+	if target_handler != null:
+		self.pressed.connect(_on_pressed_call);
+	elif target_scene != null:
+		self.pressed.connect(_on_pressed_switch_scenes)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,5 +23,9 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_pressed():
+func _on_pressed_call():
 	target_handler.emit_signal(target_signal_name, target_scene)
+	
+func _on_pressed_switch_scenes():
+	get_tree().change_scene_to_packed(target_scene)
+	
