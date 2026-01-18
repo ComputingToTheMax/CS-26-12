465f784e121c404214bfdb481b7162c4051b18ea
Avery Feingold <55460590+AveryFeingold@users.noreply.github.com>
2026-01-17 22:47:21 -0800

    Implement off-board check for character_body_2d in scenes
    Add function to check if character is off the board and change scene.

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -13,7 +13,13 @@ func _ready():
 	)
 	global_position = target
 
+func _is_off_board():
+	var board = get_viewport_rect().size
+	if target.x > board.x:
+		get_tree().change_scene_to_file("res://Scenes/end_screen.tscn")
+		
 func _process(event):
 	if Input.is_key_pressed(KEY_RIGHT):
 		target.x += snap.x
 		global_position = target
+		_is_off_board();
