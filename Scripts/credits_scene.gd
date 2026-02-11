92101ecb48497193d816a1a3c3163acd27a8129c
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-02-11 00:52:17 -0800

    Update Credits Button Setup

--- Scripts/credits_scene.gd
+++ Scripts/credits_scene.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends Control
 
-func _ready():
-	$Background/ReturnButton.pressed.connect(_on_return_pressed)
-
-func _on_return_pressed():
-	var start_scene = load("res://Scenes/main_menu.tscn")
-	get_tree().change_scene_to_packed(start_scene)
+#func _ready():
+	#$Background/ReturnButton.pressed.connect(_on_return_pressed)
+#
+#func _on_return_pressed():
+	#var start_scene = load("res://Scenes/main_menu.tscn")
+	#get_tree().change_scene_to_packed(start_scene)
