6cea047db94b3f5135ee69e27b2ce925cfee58e3
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-02-10 21:19:24 -0800

    Update player count selection scene.

--- Scripts/game_setup_ui.gd
+++ Scripts/game_setup_ui.gd
@@ -1,11 +1,39 @@
 extends Control
 
+var current_pressed_button = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	
+	# Connect Children Player Count Selection Buttons to the Below Handler
+	# TODO: Store the assumption that button names represent player counts.
+	var selection_buttons = $VBoxContainer/PlayerCountButtons
+	
+	print(selection_buttons.get_children())
+	
+	for button in selection_buttons.get_children():
+		
+		button.pressed.connect(handle_player_count_selection)
+		
+		print(button.get_signal_connection_list("pressed"))
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func handle_player_count_selection(id):
+	
+	var origin_node = instance_from_id(id)
+	GlobalSettings.number_of_players = int(origin_node.name)
+	
+	print(origin_node.name, "Pressed!")
+	
+	# "Unpress" any currently pressed button and afterwards assign the new button as currently pressed.
+	if current_pressed_button:
+			current_pressed_button.pressed = false
+			
+	current_pressed_button = origin_node
