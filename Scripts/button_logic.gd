8b1decb27ef916cd0cb4f9ed267f2a8d8d4b4535
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-02-10 22:40:48 -0800

    Test - Simplify Button Logic

--- Scripts/button_logic.gd
+++ Scripts/button_logic.gd
@@ -1,22 +1,25 @@
 extends Button
 
 
-@export var target_handler: Node
-@export var target_signal_name: String
+#@export var signal_name_to_emit: String
 
 @export var target_scene: PackedScene
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	if (target_handler == null) and (target_scene == null):
-		push_error("Oops! It looks like this button was neither given a target scene, nor a target handler (a method to do something on a press). Please select the node and provide one of these so that button presses can trigger some event.")
+	#
+	#if (signal_name_to_emit == null) and (target_scene == null):
+		##push_error("Oops! It looks like this button was neither given a target scene, nor a target handler (a method to do something on a press). Please select the node and provide one of these so that button presses can trigger some event.")
+		#push_error("Oops! It looks like this button wasn't given a target scene to switch too. Please select the node in the editor and provide one.")
+		
+	if (target_scene == null):
+		push_error("Oops! It looks like this button wasn't given a target scene to switch too. Please select the node in the editor and provide one.")
 	
 	# Automatically connect any buttons this script is applied to to the _on_pressed method
 	# to handle button presses.
-	if target_handler != null:
-		self.pressed.connect(_on_pressed_call);
+	#if signal_name_to_emit != null:
+		#self.pressed.connect(_on_pressed_call);
 	elif target_scene != null:
 		self.pressed.connect(_on_pressed_switch_scenes)
 
@@ -26,8 +29,8 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_pressed_call():
-	target_handler.emit_signal(target_signal_name, target_scene)
+#func _on_pressed_call():
+	#self.emit_signal(signal_name_to_emit, target_scene)
 	
 func _on_pressed_switch_scenes():
 	get_tree().change_scene_to_packed(target_scene)
