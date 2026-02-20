a7c5489d33b41e9f07f29110f0e1862053db3aba
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-02-19 20:39:52 -0800

    Cleanup button logic handling using updated navigator scripts.

--- Scripts/button_logic.gd
+++ Scripts/button_logic.gd
@@ -1,31 +1,23 @@
 extends Button
 
-
-
+# Specify one of the below options using the editor to select button properties.
 @export var target_scene: PackedScene
-@export var target_scene_path:String
+@export var target_scene_path: String
 
 
 func _ready() -> void:
-	
-	
-	if (target_scene == null) and (target_scene_path == ""):
+	if (target_scene == null) and (target_scene_path == "" or target_scene_path == null):
 		push_error("Oops! It looks like the button titled {name} wasn't given a target scene to switch too. Please select the node in the editor and provide one.".format({"name":self.name}))
 
-	
+	if (target_scene != null) and (target_scene_path != "" or target_scene_path != null):
+		push_warning("Oops! Both a target (packed) scene and a target scene path (string) were specified. Only one option will be used.\n\tNode: %s" % [self.name])
 
 	self.pressed.connect(_on_pressed_switch_scenes)
-
-
 	
 func _on_pressed_switch_scenes():
 	
-	if target_scene_path != "":
-		
-		Navigator.go_to(target_scene_path)
-
-
-		
+	if (target_scene != null):
+		Navigator.go_to_packed_scene(target_scene)
 	
-	
-	
+	elif (target_scene_path != null):
+		Navigator.go_to_scene_by_path(target_scene_path)
