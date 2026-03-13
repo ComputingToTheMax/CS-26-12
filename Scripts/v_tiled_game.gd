9cd96984c44173f74d2ae3bc6932ed560b299b1e
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-03-12 20:47:56 -0700

    Update tiled games to fit within the current screen size.

--- Scripts/v_tiled_game.gd
+++ Scripts/v_tiled_game.gd
@@ -4,7 +4,7 @@ extends Control
 var target_minigame_path:String
 
 @onready var subview_template = $SubViewTemplate
-@onready var subview_parent = $VBoxContainer
+@onready var subview_parent = $CenterContainer/VBoxContainer
 
 func __init(target_minigame_path: String) -> void:
 	self.target_minigame_path = target_minigame_path
@@ -14,9 +14,15 @@ func __init(target_minigame_path: String) -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
+	# Calculate the maximum height of each minigame viewport.
+	var viewport_size = GlobalSettings.get_window_size()
+	viewport_size.y = (viewport_size.y - (5 * (GlobalSettings.number_of_players - 1))) / GlobalSettings.number_of_players
+	
+	print("Viewport Size:", viewport_size)
+	
 	for player in GlobalSettings.active_players:
 		print("Creating subview!")
-		_create_subview(player)
+		_create_subview(player, viewport_size)
 		
 
 
@@ -25,7 +31,8 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _create_subview(player: GlobalSettings.PlayerConfiguration):
+func _create_subview(player: GlobalSettings.PlayerConfiguration, viewport_size: Vector2i):
+	
 	
 	print("PATH!", target_minigame_path)
 	var current_instance_of_target_scene = load(target_minigame_path).instantiate()
@@ -34,6 +41,7 @@ func _create_subview(player: GlobalSettings.PlayerConfiguration):
 	current_subview.__init(player)
 	var current_subview_viewport = current_subview.get_node("SubViewport")
 	
+	current_subview_viewport.size = viewport_size
 	current_subview_viewport.add_child(current_instance_of_target_scene)
 	
 	# Finish by making the current subview visible and adding it into the working tree.
