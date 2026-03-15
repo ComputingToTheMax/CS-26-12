9122ef1e4c78fed4ae29e738e68d959d60fc5d6b
Avery Feingold <55460590+AveryFeingold@users.noreply.github.com>
2026-03-15 12:12:44 -0700

    Add new properties to ItemData class

--- Scripts/Inventory/ItemData.gd
+++ Scripts/Inventory/ItemData.gd
@@ -3,7 +3,13 @@ class_name ItemData
 
 @export var id: String
 @export var display_name: String
+@export var mission : String
 
+@export var speed : int
+@export var durability : int
+@export var efficiency : int
+@export var time_bonus : int
+@export var difficulty_reduction : int
 @export var icon: Texture2D
 @export var value: int = 10
 @export var max_stack: int = 99
