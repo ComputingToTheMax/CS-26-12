11dacce4aa203d8599567c7d3d52e28ddd9307f5
Avery Feingold <55460590+AveryFeingold@users.noreply.github.com>
2026-03-15 12:11:58 -0700

    Added functionionality to load items through json file

---
+++ Scripts/Inventory/ItemDatabase.gd
@@ -0,0 +1,36 @@
+extends Node
+class_name ItemDatabase
+
+var items : Dictionary = {}
+
+func load_items(json_path:String):
+
+	var file = FileAccess.open(json_path, FileAccess.READ)
+	if file == null:
+		print("Failed to load items")
+		return
+
+	var text = file.get_as_text()
+	var data = JSON.parse_string(text)
+
+	if data == null:
+		print("Invalid JSON")
+		return
+
+	for item_data in data["items"]:
+
+		var item := ItemData.new()
+
+		item.id = item_data["ID"]
+		item.display_name = item_data["Name"]
+		item.description = item_data["Description"]
+		item.value = item_data["Price"]
+		item.max_stack = item_data["MaxStack"]
+		item.icon = load(item_data["Icon"])
+
+		items[item.id] = item
+
+	print("Loaded ", items.size(), " items")
+
+func get_item(id:String) -> ItemData:
+	return items.get(id)
