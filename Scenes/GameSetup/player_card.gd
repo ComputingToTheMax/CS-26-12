d1204468685cb19b2e8fd2d8431abc1a96f3031a
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-04-25 18:45:43 -0700

    Add Basic Player Card Utilities

---
+++ Scenes/GameSetup/player_card.gd
@@ -0,0 +1,43 @@
+extends VBoxContainer
+
+#
+# Code Handling Card Creation
+#
+
+@onready var card_background = %CardBackground
+@onready var card_foreground = %CardForeground
+
+static var card_number:int = 0
+
+const CARDS_TO_COLORS = {
+	0: "res://Scenes/GameSetup/Images/BluePlayerSelect.svg",
+	1: "res://Scenes/GameSetup/Images/RedPlayerSelect.svg",
+	2: "res://Scenes/GameSetup/Images/GreenPlayerSelect.svg",
+	3: "res://Scenes/GameSetup/Images/PurplePlayerSelect.svg"
+}
+
+func _set_texture_rect_texture_from_image_path(image_path, target_texture_rect):
+	var target_image = Image.load_from_file(image_path)
+	var target_texture = ImageTexture.create_from_image(target_image)
+	
+	target_texture_rect.texture = target_texture
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	# Each instantiated card should have it's own color. If necessary, repeat colors.
+	var target_image_path:String = CARDS_TO_COLORS[card_number % 4]
+	_set_texture_rect_texture_from_image_path(target_image_path, card_background)
+	card_number += 1
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+#	
+# Code Handling Card Interactions
+#
+
+@onready var coin_slot = %CoinSlot
