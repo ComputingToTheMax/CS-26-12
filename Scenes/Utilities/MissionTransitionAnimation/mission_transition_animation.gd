f61224664e875b1fd3173921c5e7dd232f586293
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-06-04 11:48:16 +0000

    Completed first pass of mission transition animation.

--- Scenes/Utilities/MissionTransitionAnimation/mission_transition_animation.gd
+++ Scenes/Utilities/MissionTransitionAnimation/mission_transition_animation.gd
@@ -1,10 +1,14 @@
 extends Control
 
+signal ready_to_transition
+
 @export var animation_duration:int = 3
 
 @onready var animation_camera = %AnimationCamera
 @onready var holding_timer = $HoldingTimer
+@onready var transition_timer = $TransisionTimer
 
+#const starting_offset: Vector2 = Vector2(4500, 300)
 const starting_offset: Vector2 = Vector2(4900, 300)
 const holding_offset: Vector2 = Vector2(2000, 300)
 const target_offset: Vector2 = Vector2(-3400, 300)
@@ -16,12 +20,17 @@ func _ready() -> void:
 	# If this scene is run independently, launch the animation.
 	if owner == null:
 		play_animation()
+	
+	#var root = get_tree().root
+	#if get_parent() != root:
+		#reparent(root)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
 func play_animation():
+	self.show()
 	animation_camera.offset = starting_offset
 	current_tween = create_tween()
 	current_tween.tween_property(animation_camera, "offset", holding_offset, animation_duration * 1 / 3).set_ease(Tween.EASE_IN_OUT)
@@ -30,4 +39,20 @@ func play_animation():
 func _end_animation():
 	holding_timer.start()
 	await holding_timer.timeout
-	create_tween().tween_property(animation_camera, "offset", target_offset, animation_duration * 2 / 3.).set_ease(Tween.EASE_IN_OUT)
+	current_tween = create_tween()
+	
+	var exit_duration: float = animation_duration * 2 / 4.
+	current_tween.tween_property(animation_camera, "offset", target_offset, exit_duration).set_ease(Tween.EASE_IN_OUT)
+	
+	transition_timer.wait_time = exit_duration * 3 / 4 + 0.1
+	transition_timer.start()
+	
+	current_tween.tween_callback(_queue_free)
+
+func _queue_free():
+	queue_free()
+
+# Emit the transition signal when the viewport is hidden and we can
+# safely transition.
+func _emit_transition_signal():
+	ready_to_transition.emit()
