80273b362918a9bc6efe7b6d440c7586427564df
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-06-04 08:55:19 +0000

    Restore original design invarients with more explicit warning.

--- Scripts/GlobalVariables/audio.gd
+++ Scripts/GlobalVariables/audio.gd
@@ -1,29 +1,43 @@
 extends Node
 
+# Invariant: All audio string names listed in the array below
+#			 will have correspondingly named nodes that support
+#			 standard playback operations guranteed by the audi_loop.gd
+#			 script.
 const AVAILABLE_AUDIO = [
 	"LiftOff!!",
 ]
 
-func play_audio(audio_name, start_from_beginning=true):
-
+func check_if_audio_exists(audio_name):
+	
 	if !(audio_name in AVAILABLE_AUDIO):
-		push_warning("Oops, an audio soundtrack named " + str(audio_name) + "wasn't found.")
+		push_warning("Oops, an audio soundtrack named " + str(audio_name) + "wasn't found in the list of available audio.")
+		return false
+	
+	# Additional guard clauses above.
+	return true
+
+func play_audio(audio_name, start_from_beginning=true):
+	if !check_if_audio_exists(audio_name):
 		return
 
 	var target_audio_node = get_node(audio_name)
-	if target_audio_node.has_method("fade_in_play"):
-		target_audio_node.fade_in_play()
+	if target_audio_node.has_method("fade_in"):
+		target_audio_node.fade_in(start_from_beginning)
 
 # Fade a soundtrack back in (alias of play_audio for readability at call sites).
-func fade_in_audio(audio_name) -> void:
-	play_audio(audio_name)
+func fade_in_audio(audio_name, start_from_beginning=true) -> void:
+	play_audio(audio_name, start_from_beginning)
 
 # Fade a soundtrack out and pause it so it can be resumed later.
 func fade_out_audio(audio_name) -> void:
-	var target_audio_node = get_node_or_null(audio_name)
-	if target_audio_node != null and target_audio_node.has_method("fade_out"):
-		target_audio_node.fade_out()
 	
+	if !check_if_audio_exists(audio_name):
+		return
+	
+	var target_audio_node = get_node(audio_name)
+	if target_audio_node.has_method("fade_out"):
+		target_audio_node.fade_out()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
