9b9d289396a62bb02e841b599d34a2038acaa129
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-06-04 11:19:18 +0000

    Update mission transition animation to avoid autoplaying itself unless run directly from the editor.

--- Scenes/Utilities/MissionTransitionAnimation/mission_transition_animation.gd
+++ Scenes/Utilities/MissionTransitionAnimation/mission_transition_animation.gd
@@ -13,7 +13,9 @@ var current_tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play_animation()
+	# If this scene is run independently, launch the animation.
+	if owner == null:
+		play_animation()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,10 +24,10 @@ func _process(delta: float) -> void:
 func play_animation():
 	animation_camera.offset = starting_offset
 	current_tween = create_tween()
-	current_tween.tween_property(animation_camera, "offset", holding_offset, animation_duration * 2 / 3).set_ease(Tween.EASE_IN_OUT)
+	current_tween.tween_property(animation_camera, "offset", holding_offset, animation_duration * 1 / 3).set_ease(Tween.EASE_IN_OUT)
 	current_tween.tween_callback(_end_animation)
 	
 func _end_animation():
 	holding_timer.start()
 	await holding_timer.timeout
-	create_tween().tween_property(animation_camera, "offset", target_offset, animation_duration / 3.).set_ease(Tween.EASE_IN_OUT)
+	create_tween().tween_property(animation_camera, "offset", target_offset, animation_duration * 2 / 3.).set_ease(Tween.EASE_IN_OUT)
