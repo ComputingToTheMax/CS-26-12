0ad84fe5b77a2acd9b8fa4309aa6bdd47c2d5e34
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-06-04 09:36:36 +0000

    Avoid duplicate coin insertions.

--- Scenes/GameSetup/coin_slot.gd
+++ Scenes/GameSetup/coin_slot.gd
@@ -2,6 +2,8 @@ extends Control
 
 signal token_dropped(token:DiscoveryToken)
 
+var token_already_dropped = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,6 +14,10 @@ func _process(delta: float) -> void:
 	pass
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+	
+	if token_already_dropped:
+		return false
+	
 	if data is DiscoveryToken:
 		data.rotate_to_side()
 		
@@ -28,3 +34,5 @@ func _drop_data(at_position: Vector2, data: Variant) -> void:
 		# Request the token to preform drop animations and behavior itself.
 		data.drop_coin()
 		token_dropped.emit(data)
+		
+		token_already_dropped = true
