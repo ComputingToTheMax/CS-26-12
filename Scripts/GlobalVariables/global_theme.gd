b8c6e6aea71ca11e080968f9daa6588042988f61
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-06-04 23:00:42 +0000

    Create global game theming resources.

---
+++ Scripts/GlobalVariables/global_theme.gd
@@ -0,0 +1,45 @@
+extends Node
+
+const DISCOVERY_MISSIONS = [
+	"Near Shoemaker",
+	"Pathfinder",
+	"Lunar Prospector",
+	"Stardust",
+	"Genesis",
+	"Contour",
+	"Messenger",
+	"Deep Impact",
+	"Dawn",
+	"Kepler Space Telescope",
+	"Grail",
+	"InSight",
+	"Lucy",
+	"Psyche"
+]
+
+const DISCOVERY_MISSION_IMAGES = {
+	"Near Shoemaker": "res://Sources/Images/Discovery Missions/01A NEAR Shoemaker.png",
+	"Pathfinder": "res://Sources/Images/Discovery Missions/02A Pathfinder.png",
+	"Lunar Prospector": "res://Sources/Images/Discovery Missions/03A Lunar Prospector.png",
+	"Stardust": "res://Sources/Images/Discovery Missions/04A Stardust.png",
+	"Genesis": "res://Sources/Images/Discovery Missions/05A Genesis.png",
+	"Contour": "res://Sources/Images/Discovery Missions/06A CONTOUR.png",
+	"Messenger": "res://Sources/Images/Discovery Missions/07A MESSENGER.png",
+	"Deep Impact": "res://Sources/Images/Discovery Missions/08A Deep Impact.png",
+	"Dawn": "res://Sources/Images/Discovery Missions/09A Dawn.png",
+	"Kepler Space Telescope": "res://Sources/Images/Discovery Missions/10A Kepler Space Telescope.png",
+	"Grail": "res://Sources/Images/Discovery Missions/11A GRAIL.png",
+	"InSight": "res://Sources/Images/Discovery Missions/12A InSight.png",
+	"Lucy": "res://Sources/Images/Discovery Missions/13A Lucy.png",
+	"Psyche": "res://Sources/Images/Discovery Missions/14A Psyche.png"
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
