e41ec194549274e64420770f29987239bab29de1
ComputingToTheMax <238603684+ComputingToTheMax@users.noreply.github.com>
2026-06-05 07:43:09 +0000

    Update the Genesis Solar Wind Minigame with Player Instructions and Feedback.

--- Minigames/GenesisSolarWind/Scenes/genesis_solar_wind_minigame.gd
+++ Minigames/GenesisSolarWind/Scenes/genesis_solar_wind_minigame.gd
@@ -1,17 +1,30 @@
 extends Node2D
 
-@export var target_game_tile_scene_path: String
+signal done(result: Dictionary)
 
+@export var target_game_tile_scene_path: String
 @export var stacking_organizing_scene_path: String
+
+var game_handler
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	var loaded_handler = load(stacking_organizing_scene_path).instantiate()
-	loaded_handler.__init(get_tree(), target_game_tile_scene_path)
+	game_handler = load(stacking_organizing_scene_path).instantiate()
+	
+	game_handler.game_done.connect(_propagate_game_done_signal)
+	
+	game_handler.__init(get_tree(), target_game_tile_scene_path)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _propagate_game_done_signal(result: Dictionary):
+
+	print("Exiting the Genesis Solar Wind Minigame!")
+	#game_handler.queue_free()
+	done.emit(result)
+	queue_free()
